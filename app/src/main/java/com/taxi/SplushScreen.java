aee9ad5deacbb361e1b118dde786793d444917fe
AMD21 <bhadreshchavada@gmail.com>
2017-05-13 11:15:18 +0530

    Initial commit

---
+++ app/src/main/java/com/taxi/SplushScreen.java
@@ -0,0 +1,13 @@
+package com.taxi;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplushScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splush_screen);
+    }
+}
